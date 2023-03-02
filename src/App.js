6264ebf120ff1b74ad33fc3c511d67a50b37d139
juromerop <juromerop@unal.edu.co>
2023-03-02 11:57:01 -0500

    Trying some media querys on title

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       marginTop={25}
       >
         <Grid2 container spacing={5}>
-          <Grid2 xs={8}>
+          <Grid2 xs={8} className="Title">
             <ToDoTitle />
             <ToDoCounter />
           </Grid2>
